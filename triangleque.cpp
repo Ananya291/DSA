153d02f9555bc2d4fbcbba0fe9a0a13eec29a261
Ananya Thakur <75697464+Ananya291@users.noreply.github.com>
2021-06-11 00:49:47 +0530

    Add files via upload

---
+++ triangleque.cpp
@@ -0,0 +1,21 @@
+// Given a three  number each number represent the length of a line . you need to figure out wether these lines can 
+// form a valid triangle.
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout<<"Enter any three numbers: ";
+    cin>>a>>b>>c;
+
+    if(a+b>=c || b+c>=a || c+a>=b){
+        cout<<"Yes"<<endl;
+    }
+    else
+    cout<<"No"<<endl;
+
+    return 0;
+
+}
