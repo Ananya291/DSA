70b8978061b510e2d776937841d75338801c784d
Ananya Thakur <75697464+Ananya291@users.noreply.github.com>
2021-06-14 03:32:19 +0530

    Update Characterinput.cpp

--- Characterinput.cpp
+++ Characterinput.cpp
@@ -1,3 +1,4 @@
+// WAP that takes a character input and print 1 if the character is an uppercase(A-Z), print 0 if the character is a lowercase(a-z) and print -1 if the character is not alpha.
 #include <iostream>
 using namespace std;
 
