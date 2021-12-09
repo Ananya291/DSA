b446e73712792998f6ed3c489f0a4b386f3be3f8
Ananya Thakur <75697464+Ananya291@users.noreply.github.com>
2021-12-09 14:56:43 +0530

    Add files via upload

---
+++ staticVariable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void fun(){
+    static int x = 1;
+    int y = 1;
+    x++;
+    y++;
+    cout<<x<<" "<<y<<endl;
+
+}
+
+int main(){
+    fun();
+    fun();
+    fun();
+    return 0;
+}
