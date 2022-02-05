c77cb33f564a46ebd7d8ebc948ed0442e70a517d
Ananya Thakur <75697464+Ananya291@users.noreply.github.com>
2022-02-05 23:23:46 +0530

    Add files via upload

---
+++ Findaverageinanarray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+double find_average(int arr[], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+    }
+    return sum / double(n);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << find_average(arr, n);
+    return 0;
+}
