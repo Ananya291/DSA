849e50cb14fea65cdda537dc1794f5b0bec78445
Ananya Thakur <75697464+Ananya291@users.noreply.github.com>
2022-02-06 00:26:26 +0530

    Add files via upload

---
+++ Referenceinc++3.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector <string> vect{"hey", "there"};
+    for (const auto &x : vect)
+    {
+        cout << x <<" " ;
+    }
+    return 0;
+}
