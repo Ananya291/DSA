9e26003c39ba909821b163315690968fd5ae8343
Ananya Thakur <75697464+Ananya291@users.noreply.github.com>
2022-02-05 23:38:58 +0530

    Add files via upload

---
+++ ReferenceinC++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 10;
+    int &y = x;
+
+    cout<<y<<endl;
+    x = x+5;
+    cout<<y<<endl;
+    y = y+5;
+    cout<<x<<endl;
+    return 0;
+}
