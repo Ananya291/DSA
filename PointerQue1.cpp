3eb434c4c7713b6264f5f8290f9c0ff0561f9547
Ananya Thakur <75697464+Ananya291@users.noreply.github.com>
2022-02-06 23:34:48 +0530

    Add files via upload

---
+++ PointerQue1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    int *p;
+    p = &x;
+    cout << x << endl;
+    cout << *p << endl;
+    cout << p << endl;
+    return 0;
+}
