f8dd05deee7cfb6a2695b741cb152530a9b14053
Ananya Thakur <75697464+Ananya291@users.noreply.github.com>
2022-02-06 23:16:31 +0530

    Add files via upload

---
+++ Reference4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int &fun()
+{
+    static int x = 10;
+    return x;
+}
+
+int main()
+{
+    int &y = fun();
+    y = 20;
+    cout << fun();
+    return 0;
+}
