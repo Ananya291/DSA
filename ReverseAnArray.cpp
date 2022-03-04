292eeaca56b1ae5184afd261a5ff0e3a41e1dffe
Ananya Thakur <75697464+Ananya291@users.noreply.github.com>
2022-03-04 16:33:31 +0530

    Add files via upload

---
+++ ReverseAnArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void reverse(int arr[],int n){
+    
+    int low = 0;
+    int high = n-1;
+    while(low<high){
+        int temp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp;
+        low++;
+        high--;
+    }
+}
+
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    reverse(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
