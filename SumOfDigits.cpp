db659ea82793101b49b9e5a844a84406a14dd31a
Ananya Thakur <75697464+Ananya291@users.noreply.github.com>
2022-03-15 18:28:08 +0530

    Add files via upload

---
+++ SumOfDigits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int fun(int n)
+{
+	if(n < 10)
+		return n;
+
+	return fun(n / 10) + n % 10;
+}
+
+int main() {
+	
+	cout<<fun(253);
+	
+	return 0;
+}
